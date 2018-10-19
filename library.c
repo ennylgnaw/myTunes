efe6dc0c9203441c464beaad07cf2f0071d9ff1c
ladyt <egulsen@stuy.edu>
2018-10-18 22:58:54 -0400

    Added a bunch of functions

--- library.c
+++ library.c
@@ -13,7 +13,6 @@ struct music_lib * addnode(struct music_lib * library, char songname[100], char 
       int i;
       for (i=0; i<=26; i++) library->table[i] = NULL;
   }
-  if (!library->table[songartist[0]-'a'])
   library->table[songartist[0] -'a'] = insert_order(library->table[songartist[0]-'a'], songname, songartist);
   return library;
 }
@@ -25,6 +24,24 @@ void print_library(struct music_lib * library) {
     }
 }
 
+struct song_node * song_search(struct music_lib * library, char songname[100], char songartist[100]) {
+    return find_song(library->table[songartist[0] - 'a'], songname, songartist);
+}
+
+void artist_search(struct music_lib * library, char songartist[100]){
+    struct song_node * current = find_artist(library->table[songartist[0]-'a'], songartist);
+    if (current) printf("Found %s: ", songartist);
+    while (current) {
+        printf("%s by %s - > ", current->name, songartist);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+void print_letter(struct music_lib * library, char letter) {
+    print_list(library->table[letter - 'a']);
+}
+
 int main() {
   struct music_lib * lib1 = addnode(NULL, "love story", "taylor swift");
   lib1 = addnode(lib1, "peek-a-boo", "red velvet");
@@ -32,4 +49,8 @@ int main() {
   lib1 = addnode(lib1, "pocket crocodile", "kero kero bonito");
   lib1 = addnode(lib1, "automatic", "red velvet");
   print_library(lib1);
+  print_list(song_search(lib1, "automatic", "red velvet"));
+  artist_search(lib1, "red velvet");
+  print_letter(lib1, 'k');
+  
 }
